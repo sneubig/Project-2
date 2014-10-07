7170683628169e96c8cee1426d42d794a543dc2b
pmills88 <pmillerk88@gmail.com>
2014-10-07 14:52:42 -0400

    Bugfixes

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
-
 	$('.nav-icon p').click(function(){
 		$('.side-menu').toggleClass('extended');
 	})
-
-	// $('.nav-icon p').on('click', function(){
-	// 	$('.side-menu').toggleClass(".extended");
-	// })
-
-
-
 });
