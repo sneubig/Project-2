d14f1fd3ba550c7a091f314fcc757fd0bc84d6a5
pmills88 <pmillerk88@gmail.com>
2014-10-07 10:04:51 -0400

    Further modelling

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
 
   def find_long_lat
   	raw_data = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{self.zipcode}&key=#{Rails.application.secrets.google_api_key}")
-
   	self.longitude = raw_data["results"][0]["geometry"]["location"]["lng"]
   	self.latitude = raw_data["results"][0]["geometry"]["location"]["lat"]
   	self.save
@@ -17,8 +16,10 @@ class User < ActiveRecord::Base
 
   def find_weather
   	raw_data = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{self.latitude}&lon=#{self.longitude}")
-  	temp = (9/5) * (raw_data["main"]["temp"].to_f - 273) + 32
+  	self.min_temp = (9/5) * (raw_data["main"]["temp_min"].to_f - 273) + 32
+    self.max_temp = (9/5) * (raw_data["main"]["temp_max"].to_f - 273) + 32   
+    self.save
 
-    
+
   end
 end
