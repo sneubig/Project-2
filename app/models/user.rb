bef107cb13ec69a7e8bd461d436575091e308c10
Elwend <elizabeth.wendling@gmail.com>
2014-10-07 12:16:05 -0400

    Added messaging.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,28 +35,6 @@ class User < ActiveRecord::Base
       return puts "Take off your sweatshirt"
     end
   end
-
-  #Here we can define variables based on the different data 
-  #we will want in the text_body below
-  
-  def send_message
-    require 'rubygems'         
-    require 'twilio-ruby'
-
-    users = User.all
-    
-    #Loop through user data to get the data you want for the text
-    users.each do |user|
-      #Format user cell to work with Twilio
-      number = "+1#{user.cell}"
-      text_body = "Hey there"
-
-      @client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-      message = @client.account.messages.create(
-        :body =>  text_body,
-        :to => number,
-        :from => "+12027514370"
-        )
-    end 
-  end
 end
+
+
