4c346c11fde6cea1f9c841242e9280950fffbf40
Elwend <elizabeth.wendling@gmail.com>
2014-10-08 10:00:25 -0400

    Working on finding/sending movie info

--- app/models/message.rb
+++ app/models/message.rb
@@ -35,6 +35,36 @@ class Message
     send_message(x, "Testestestest")
   end
 
+   #Find movies using the Rotten Tomatos API
+  def sunny_movies_message
+    address = URI("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?limit=16&country=us&apikey=" + ENV["tomato_key"])
+    http = Net::HTTP.new address.host, address.port
+    request = Net::HTTP::Get.new address.request_uri
+    response = http.request(request)
+    result = JSON.parse(response.body)
+    movies = result["movies"]
+    chosenMovies = []
+
+    movies.each do |movie|
+      if movie["ratings"]["critics_score"] > 70
+        chosenMovies.push(movie)
+      end
+    end
+    
+    movies_text = ""
+    chosenMovies.each do |movie|
+      @movies_text += movie
+    end
+
+    @showTimes = "http://www.fandango.com/"
+    movies_text_body = "#{@movies_text}#{@showTimes}#{zipcode}_movietimes?q=#{zipcode}"
+
+    users = User.where(desc: "clear")
+    users.each do |x|
+      send_message(x, movies_text_body)
+    end
+  end
+
   def send_message(user, text)
     require 'rubygems'
     require 'twilio-ruby'
